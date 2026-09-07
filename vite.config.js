b16e6c714a772a06bfebb029fbdb5b761272d289
Shubham Sourav <110170581+shubhamjha5@users.noreply.github.com>
2026-09-07 09:37:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-
-  // GitHub repository name:
-  // https://github.com/<your-username>/Online_CBT
-  base: "/Online_CBT/"
-});
+  base: command === "build" ? "/Online_CBT/" : "/",
+}));
